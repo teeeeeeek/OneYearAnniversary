dc18a7a33f45511e3ec5ef53a31630942de54ac6
teeeeeeek <askarteek@gmail.com>
2025-05-17 19:19:43 -0400

    Add files via upload

---
+++ photos.py
@@ -0,0 +1,17 @@
+# photos.py
+
+# Optional stub file if you need compatibility with:
+# from photos import get_couple_photos, get_date_photos, get_landscape_photos
+
+# Since you're using local images, you can leave these as empty placeholders
+# or have them return predefined fallback lists (in case of legacy use)
+
+def get_couple_photos():
+    return [
+        "D:\OneYearAnniversary\images\farewell1.jpg",
+        "D:\OneYearAnniversary\images\farewell2.jpg",
+        "D:\OneYearAnniversary\images\lunch1.jpg",
+        "D:\OneYearAnniversary\images\lunch2.jpg",
+        "D:\OneYearAnniversary\images\nights1.jpg",
+        "D:\OneYearAnniversary\images\nights2.jpg"
+    ]
